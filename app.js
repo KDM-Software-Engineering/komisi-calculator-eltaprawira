635c4aec8c733fabd1e0a4d43b23c4e9bb6edd21
eltaprawira <lestarielta649@gmail.com>
2024-10-24 14:26:39 +0700

    selesai

--- app.js
+++ app.js
@@ -10,6 +10,19 @@
 
 // TULIS SCRIPT KAMU DI SINI
 
+function calculateKomisi (nilaiKontrak) {
+  if (nilaiKontrak >= 5000000) {
+    return nilaiKontrak * 0.05;
+    }
+
+    else if ( nilaiKontrak >= 1000000)
+      return 100000;
+
+    else{
+      return 0;
+    }
+ 
+}
 
 
 // JANGAN HAPUS / UBAH SCRIPT DI BAWAH
